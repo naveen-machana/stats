f9aacec80ed46c9e5a9827aa5f880fabbd2123d5
Naveen <naveenou@gmail.com>
2019-10-09 16:11:46 +0530

    second version of csvfilereader-composition

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,25 +1,32 @@
-import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from './MatchResult';
 import { dateStringToDate } from './utils';
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-export class MatchReader extends CsvFileReader<MatchData> {
-  constructor(filename: string) {
-    super(filename);
-  }
+interface DataReader {
+  read(): void;
+  data: string[][];
+}
 
-  mapRow(
-    row: string[]
-  ): [Date, string, string, number, number, MatchResult, string] {
-    return [
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult,
-      row[6]
-    ];
+export class MatchReader {
+  matches: MatchData[] = [];
+
+  constructor(public reader: DataReader) {}
+
+  load(): void {
+    this.reader.read();
+    this.matches = this.reader.data.map(
+      (row: string[]): MatchData => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult,
+          row[6]
+        ];
+      }
+    );
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
+import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+const matchReader = new MatchReader(new CsvFileReader('football.csv'));
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
